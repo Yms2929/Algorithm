09bbf90694f0a1076d6b77024d5ef251e5ba1ed4
DESKTOP-VMUUAM3\Yookmoonsu <yookmoon@naver.com>
2018-04-02 22:33:54 +0900

    문자열 반복

--- src/algorithmstudy/Practice.java
+++ src/algorithmstudy/Practice.java
@@ -1,16 +1,16 @@
-package algorithmstudy;
-
-import java.util.Scanner;
-
-public class Practice {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		Scanner scanner = new Scanner(System.in);
-		char character;
-		character = scanner.next().charAt(0); // 문자 입력
-		
-		System.out.println(Integer.valueOf(character)); // 문자를 int형으로 변환
-	}
-	
-}
+//package algorithmstudy;
+//
+//import java.util.Scanner;
+//
+//public class Practice {
+//
+//	public static void main(String[] args) {
+//		// TODO Auto-generated method stub
+//		Scanner scanner = new Scanner(System.in);
+//		char character;
+//		character = scanner.next().charAt(0); // 문자 입력
+//		
+//		System.out.println(Integer.valueOf(character)); // 문자를 int형으로 변환
+//	}
+//	
+//}
