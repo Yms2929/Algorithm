294cd6e9810b7b1d0a5ccdb1f3448a141679fbf0
DESKTOP-VMUUAM3\Yookmoonsu <yookmoon@naver.com>
2018-04-14 22:42:23 +0900

    좌표 정렬

--- src/algorithmstudy/NumberSort.java
+++ src/algorithmstudy/NumberSort.java
@@ -1,24 +1,24 @@
-package algorithmstudy;
-
-import java.util.Arrays;
-import java.util.Scanner;
-
-public class NumberSort {
-
-	public static void main(String[] args) {
-		Scanner scanner = new Scanner(System.in);
-		int N = scanner.nextInt();
-		int[] number = new int[N];
-		
-		for (int i = 0; i < N; i++) {
-			number[i] = scanner.nextInt();
-		}
-		
-		Arrays.sort(number);
-		
-		for (int i = 0; i < N; i++) {
-			System.out.println(number[i]);
-		}
-	}
-
-}
+//package algorithmstudy;
+//
+//import java.util.Arrays;
+//import java.util.Scanner;
+//
+//public class NumberSort {
+//
+//	public static void main(String[] args) {
+//		Scanner scanner = new Scanner(System.in);
+//		int N = scanner.nextInt();
+//		int[] number = new int[N];
+//		
+//		for (int i = 0; i < N; i++) {
+//			number[i] = scanner.nextInt();
+//		}
+//		
+//		Arrays.sort(number);
+//		
+//		for (int i = 0; i < N; i++) {
+//			System.out.println(number[i]);
+//		}
+//	}
+//
+//}
