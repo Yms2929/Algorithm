322d6c8e2f92f0b235d6b9caab097f3c9f873dd5
DESKTOP-VMUUAM3\Yookmoonsu <yookmoon@naver.com>
2018-04-20 00:15:59 +0900

    1로 만들기 Bottom Up

--- src/algorithmstudy/OneToMake.java
+++ src/algorithmstudy/OneToMake.java
@@ -1,20 +1,33 @@
-//package algorithmstudy;
-//
-//import java.util.Scanner;
-//
-//public class OneToMake {
-//	
-//	public static int d[];
-//	public static void main(String[] args) {
-//		Scanner scanner = new Scanner(System.in);
-//		int N = scanner.nextInt();
-//		d = new int[N+1];
-//		// D[N] = N을 1로 만드는데 필요한 연산의 최소값
+package algorithmstudy;
+
+import java.util.Scanner;
+
+public class OneToMake {
+	
+	public static int d[];
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		int N = scanner.nextInt();
+		d = new int[N+1];
+		// D[N] = N을 1로 만드는데 필요한 연산의 최소값
 //		System.out.println(oneToMake(N));
-//	}
-//	
-//	// N을 N/3으로 바꾸는데 1번 N/3을 1로 만드는데 D[N/3]번이므로
-//	// D[N/3] + 1 이 된다
+		
+		d[1] = 0;
+		for (int i = 2; i <= N; i++) {
+			d[i] = d[i-1] + 1;
+			if (i % 2 == 0 && d[i] > d[i/2] + 1) {
+				d[i] = d[i/2] + 1;
+			}
+			if (i % 3 == 0 && d[i] > d[i/3] + 1) {
+				d[i] = d[i/3] + 1;
+			}
+		}
+		
+		System.out.println(d[N]);
+	}
+	
+	// N을 N/3으로 바꾸는데 1번 N/3을 1로 만드는데 D[N/3]번이므로
+	// D[N/3] + 1 이 된다
 //	public static int oneToMake(int n) {
 //		if (n == 1) {
 //			return 0;
@@ -37,4 +50,4 @@
 //		}
 //		return d[n];
 //	}
-//}
+}
--- src/algorithmstudy/TwoTiling.java
+++ src/algorithmstudy/TwoTiling.java
@@ -1,22 +1,23 @@
-package algorithmstudy;
-
-import java.util.Scanner;
-
-public class TwoTiling {
-		
-	public static void main(String[] args) {
-		Scanner scanner = new Scanner(System.in);
-		int n = scanner.nextInt();
-		int d[] = new int[1001];
-		d[0] = 1;
-		d[1] = 1;
-		
-		if (1 <= n && n <= 1000) {
-			for (int i = 2; i <= n; i++) {
-				d[i] = d[i-1] + d[i-2];
-			}
-		}
-		
-		System.out.println(d[n]);
-	}
-}
+//package algorithmstudy;
+//
+//import java.util.Scanner;
+//
+//public class TwoTiling {
+//		
+//	public static void main(String[] args) {
+//		Scanner scanner = new Scanner(System.in);
+//		int n = scanner.nextInt();
+//		int d[] = new int[1001];
+//		d[0] = 1;
+//		d[1] = 1;
+//		
+//		if (1 <= n && n <= 1000) {
+//			for (int i = 2; i <= n; i++) {
+//				d[i] = d[i-1] + d[i-2];
+//				d[i] = d[i] % 10007;
+//			}
+//		}
+//		
+//		System.out.println(d[n]);
+//	}
+//}
