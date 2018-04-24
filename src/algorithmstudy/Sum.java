55ae6f93480308a957568d0a9b78e98d7ff747fa
DESKTOP-VMUUAM3\Yookmoonsu <yookmoon@naver.com>
2018-04-24 16:16:15 +0900

    요일구하기

--- src/algorithmstudy/Sum.java
+++ src/algorithmstudy/Sum.java
@@ -1,36 +1,48 @@
-package algorithmstudy;
-
-import java.util.Arrays;
-import java.util.Scanner;
-
-public class Sum {
-
-	public static void main(String[] args) {
-		Scanner scanner = new Scanner(System.in);
-		int N = scanner.nextInt();
-		int[] number = new int[N];
-		
-		for (int i = 0; i < N; i++) {
-			number[i] = scanner.nextInt();
-		}
-		
-		Arrays.sort(number);
-		
-		System.out.println(number[0] + " " + number[number.length - 1]);
-	}
-	
+//package algorithmstudy;
+//
+//import java.util.Scanner;
+//
+//public class Sum {
+//
 //	public static void main(String[] args) {
 //		Scanner scanner = new Scanner(System.in);
-//		int n = scanner.nextInt();
-//		int sum = 0;
+//		String N = scanner.nextLine();
 //		
-//		if (1 <= n && n <= 10000) {
-//			for (int i = 1; i <= n; i++) {
-//				sum = sum + i;
+//		for (int i = 0; i < N.length(); i++) {
+//			System.out.print(N.charAt(i));
+//			if (i % 10 == 9) {
+//				System.out.println();
 //			}
 //		}
-//		
-//		System.out.println(sum);
+//
 //	}
-
-}
+//
+//	// public static void main(String[] args) {
+//	// Scanner scanner = new Scanner(System.in);
+//	// int N = scanner.nextInt();
+//	// int[] number = new int[N];
+//	//
+//	// for (int i = 0; i < N; i++) {
+//	// number[i] = scanner.nextInt();
+//	// }
+//	//
+//	// Arrays.sort(number);
+//	//
+//	// System.out.println(number[0] + " " + number[number.length - 1]);
+//	// }
+//
+//	// public static void main(String[] args) {
+//	// Scanner scanner = new Scanner(System.in);
+//	// int n = scanner.nextInt();
+//	// int sum = 0;
+//	//
+//	// if (1 <= n && n <= 10000) {
+//	// for (int i = 1; i <= n; i++) {
+//	// sum = sum + i;
+//	// }
+//	// }
+//	//
+//	// System.out.println(sum);
+//	// }
+//
+//}
--- src/algorithmstudy/TwoThousandSevenYear.java
+++ src/algorithmstudy/TwoThousandSevenYear.java
@@ -1,9 +1,42 @@
-//package algorithmstudy;
-//
-//public class TwoThousandSevenYear {
-//
-//	public static void main(String[] args) {
-//		
-//	}
-//
-//}
+package algorithmstudy;
+
+import java.util.Scanner;
+
+public class TwoThousandSevenYear {
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		int x = scanner.nextInt();
+		int y = scanner.nextInt();
+		char date[] = new char[] { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+		String today = "";
+		int count = 0;
+
+		for (int i = 0; i < x - 1; i++) {
+			count += date[i];
+		}
+		count += y;
+
+		if (1 <= x && x <= 12) {
+			if (1 <= y && y <= 31) {
+				if (count % 7 == 0)
+					today = "SUN";
+				else if (count % 7 == 1)
+					today = "MON";
+				else if (count % 7 == 2)
+					today = "TUE";
+				else if (count % 7 == 3)
+					today = "WED";
+				else if (count % 7 == 4)
+					today = "THU";
+				else if (count % 7 == 5)
+					today = "FRI";
+				else if (count % 7 == 6)
+					today = "SAT";
+			}
+		}
+
+		System.out.println(today);
+	}
+
+}
