15c49240a3d00165242e51fb4cdd327735fb54d6
DESKTOP-VMUUAM3\Yookmoonsu <yookmoon@naver.com>
2018-10-01 16:28:11 +0900

    8진수 2진수 변환

--- src/algorithm/SystemChange.java
+++ src/algorithm/SystemChange.java
@@ -7,6 +7,38 @@ import java.util.StringTokenizer;
 
 public class SystemChange {
 
+	// 8진수 2진수 변환
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		StringBuilder sb = new StringBuilder();
+		
+		String N = st.nextToken();
+		char[] ch = N.toCharArray();
+		
+		if (ch[0] == '0') sb.append("0");
+		else if (ch[0] == '1') sb.append("1");
+		else if (ch[0] == '2') sb.append("10");
+		else if (ch[0] == '3') sb.append("11");
+		else if (ch[0] == '4') sb.append("100");
+		else if (ch[0] == '5') sb.append("101");
+		else if (ch[0] == '6') sb.append("110");
+		else if (ch[0] == '7') sb.append("111");
+		
+		for (int i = 1; i < ch.length; i++) {
+			if (ch[i] == '0') sb.append("000");
+			else if (ch[i] == '1') sb.append("001");
+			else if (ch[i] == '2') sb.append("010");
+			else if (ch[i] == '3') sb.append("011");
+			else if (ch[i] == '4') sb.append("100");
+			else if (ch[i] == '5') sb.append("101");
+			else if (ch[i] == '6') sb.append("110");
+			else if (ch[i] == '7') sb.append("111");
+		}	
+		
+		System.out.println(sb.toString());
+	}
+	
 	// 2진수 8진수로 변환
 	/*public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
