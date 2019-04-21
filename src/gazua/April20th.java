8f94c142fa1155f912bd7b10e0833ed99c7f68de
DESKTOP-VMUUAM3\Yookmoonsu <yookmoon@naver.com>
2019-04-21 16:34:01 +0900

    4월20일

--- src/gazua/April20th.java
+++ src/gazua/April20th.java
@@ -6,13 +6,46 @@ import java.util.Comparator;
 public class April20th {
 
 	public static void main(String[] args) {
-		String[] strings = {"sun","bed","car"};
+		/*String[] strings = {"sun","bed","car"};
 		strings = mySort(strings, 1);
 		for (int i = 0; i < strings.length; i++) {
 			System.out.println(strings[i]);
+		}*/
+		
+		String answer = "try hello world";
+		System.out.println(makeWord(answer));
+	}
+	
+	// 이상한 문자 만들기
+	public static String makeWord(String s) {
+		String answer = "";
+
+		String[] str = s.split("\\s");
+		StringBuilder sb = new StringBuilder();
+		
+		for (int i = 0; i < str.length; i++) {
+
+			for (int j = 0; j < str[i].length(); j++) {
+				//String temp = str[i].substring(j, j + 1);
+				//String temp = String.valueOf(str[i].charAt(j));
+				if (j % 2 == 0) {
+					//temp = temp.toUpperCase();
+					sb.append(String.valueOf(str[i].charAt(j)).toUpperCase());
+				}
+				else {
+					//temp = temp.toLowerCase();
+					sb.append(String.valueOf(str[i].charAt(j)).toLowerCase());
+				}
+				//answer += temp;
+			}
+			
+			if (i < str.length - 1) {
+				//answer += " ";
+				sb.append(" ");
+			}
 		}
 		
-		
+		return answer;
 	}
 
 	// 문자열 내마음대로 정렬하기
