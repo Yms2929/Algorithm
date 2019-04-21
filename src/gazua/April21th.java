bb94257c036a167f693a9d9ca4f76dbfca92aa74
DESKTOP-VMUUAM3\Yookmoonsu <yookmoon@naver.com>
2019-04-21 22:16:25 +0900

    짝수와 홀수, 제일 작은 수 제거하기, 정수 제곱근 판별, 자연수 뒤집어 배열로 만들기

--- src/gazua/April21th.java
+++ src/gazua/April21th.java
@@ -1,6 +1,7 @@
 package gazua;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
@@ -12,10 +13,91 @@ public class April21th {
 	
 		//System.out.println(sortDEC(118372));
 	
+		//getArray(12345);
+	
+		//System.out.println(getPOW(3));
 		
+		//removeNumber(new int[] {4,3,2,1});
+		
+		//System.out.println(getNumber(0));
 	}
 	
+	// 짝수와 홀수
+	public static String getNumber(int num) {
+
+		return (num % 2 == 0) ? "Even" : "Odd";
+	}
+	
+	// 제일 작은 수 제거하기
+	public static int[] removeNumber(int[] arr) {
+		int[] answer = {};
 		
+		if (arr.length == 1) {
+			answer = new int[1];
+			answer[0] = -1;
+			return answer;
+		}
+		else {
+			answer = new int[arr.length - 1];
+			int[] temp = new int[arr.length];
+			
+			for (int i = 0; i < arr.length; i++) {
+				temp[i] = arr[i];
+			}
+			
+			Arrays.sort(temp);
+			
+			int min = temp[0];
+			int index = 0;
+			
+			for (int i = 0; i < arr.length; i++) {
+				if (min == arr[i]) {
+					continue;
+				}
+				answer[index] = arr[i];
+				index++;
+			}
+		}
+		
+		for (int j = 0; j < answer.length; j++) {
+			System.out.print(answer[j]);
+		}
+		
+		return answer;
+	}
+	
+	// 정수 제곱근 판별
+	public static long getPOW(long n) {
+		long answer = 0;
+		
+		answer = (long) Math.sqrt(n);
+		
+		if (Math.pow(answer, 2) == n) {
+			return answer = (long) Math.pow(answer+1, 2);
+		}
+		else {
+			answer = -1;
+		}
+				
+		return answer;
+	}
+	
+	// 자연수 뒤집어 배열로 만들기
+	public static int[] getArray(long n) {
+		int[] answer = {};
+		
+		String s = Long.toString(n);
+		answer = new int[s.length()];
+		
+		int index = 0;
+		while (n > 0) {
+			answer[index] = (int) (n % 10);
+			n /= 10;
+			index++;
+		}
+				
+		return answer;
+	}
 
 	// 정수 내림차순으로 배치하기
 	public static long sortDEC(long n) {
